c475f24d3c82100415b02cd6a75929b73d23d27e
MaryArPr <prohorenko_mariy@mail.ru>
2020-10-20 19:40:19 +0300

    Задание 4

--- src/test.java
+++ src/test.java
@@ -1,5 +1,17 @@
 public class test {
-        public static void main(String[] args) {
-            System.out.println("Hello, world!");
+     public static void main(String[] args) {
+
+         int a = 1543;
+         //Ввести в консоли число в бинарном формате
+         System.out.println(Integer.toBinaryString(a));
+
+         //Перевести его в десятичный формат, записать в переменную int
+         int i = Integer.parseInt(Integer.toBinaryString(a), 2);
+         //вывести на экран
+         System.out.println(i);
+
         }
     }
+
+
+
