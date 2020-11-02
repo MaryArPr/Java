6c65b5e96cae78ee773f7d18d5640e9cd1d08d87
MaryArPr <prohorenko_mariy@mail.ru>
2020-11-02 18:04:34 +0300

    Задание 11

---
+++ src/task11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class task11 {
+
+    public static void main(String args[]) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Необходимо произвести ввод строки с клавиатуры и записать в строковую переменную. Результат вывести на экран.");
+        String text = input.nextLine();
+
+        System.out.println("Введенная строка");
+        System.out.println(text);
+
+
+
+    }
+}
